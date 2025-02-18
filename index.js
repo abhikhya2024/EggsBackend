768c88898105886402ca96069fb8344a2e27a087
abhikhya2024 <abhikhya2024>
2025-02-18 19:41:27 +0530

    test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = 8000;
