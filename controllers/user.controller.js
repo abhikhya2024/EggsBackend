27d5dddffc384cbc02d88135a0504d34c32e11a9
abhikhya2024 <abhikhya2024>
2025-02-19 14:41:34 +0530

    test

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -81,3 +81,54 @@ exports.matchOtp=(async (req, res) => {
     }
     return res.status(400).json({success: false});
   });
+
+  exports.onPinLogin = (async (req, res) => {
+    try {
+      const {pin, deviceId} = req.body;
+      console.log('Pin:', pin, 'Device ID:', deviceId);
+      // Check if the mobile is already registered
+      const user = await User.findOne({pin: pin});
+      console.log('User:', user);
+      if (user) {
+        // If passwords match, generate a session or JWT token, and respond to the user
+        return res.status(200).json({success: true, message: 'Login successful'});
+      } else {
+        // If passwords don't match, return an error
+        return res.status(400).json({success: false, message: 'Invalid pin'});
+      }
+  
+      // Send verification SMS using Twilio
+    } catch (error) {
+      console.log('Error during login:', error); // Debugging statement
+      res.status(500).json({message: 'Login failed'});
+    }
+  });
+
+  exports.passwordLogin = (async (req, res) => {
+    try {
+      const {password, deviceId} = req.body;
+      console.log('Passowrd:', password, 'Device ID:', deviceId);
+      // Check if the mobile is already registered
+      const user = await User.findOne({deviceId: deviceId});
+      console.log('user password', user.password);
+      try {
+        const match = await bcrypt.compare(password, user.password);
+        if (match) {
+          return res
+            .status(200)
+            .json({success: true, message: 'Login successful'});
+        } else {
+          return res
+            .status(400)
+            .json({success: false, message: 'Password is incorrect'});
+        }
+      } catch (error) {
+        console.error('Error verifying password:', error);
+      }
+  
+      // Send verification SMS using Twilio
+    } catch (error) {
+      console.log('Error during login:', error); // Debugging statement
+      res.status(500).json({message: 'Login failed'});
+    }
+  });
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, matchOtp, createPin } = require('../controllers/user.controller'); // Import the controller
+const { register, matchOtp, createPin, onPinLogin, passwordLogin} = require('../controllers/user.controller'); // Import the controller
 
 // @route   POST /api/auth/login
 // @desc    Login user and get JWT token
@@ -8,6 +8,8 @@ const { register, matchOtp, createPin } = require('../controllers/user.controlle
 router.post('/register', register);
 router.post('/matchOtp', matchOtp);
 router.post('/createPin', createPin);
+router.post('/onPinLogin', onPinLogin);
+router.post('/passwordLogin', passwordLogin);
 
 module.exports = router;
 
