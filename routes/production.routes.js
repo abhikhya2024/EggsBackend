7a942cae5aab56ffc4dc6b5044a284f9aec4ba22
abhikhya2024 <abhikhya2024>
2025-03-06 12:20:59 +0530

    Test commit

--- routes/production.routes.js
+++ routes/production.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createProduction } = require('../controllers/production.controller'); // Import the controller
-
+const { createProduction, getProduction } = require('../controllers/production.controller'); // Import the controller
 
 router.post('/createProduction', createProduction);
+router.get("/production", getProduction);
+
 module.exports = router;
 
